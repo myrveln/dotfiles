9d487ea0f5a474ceab8b9a36ffa4454517a4b1b3
Kim Brandt <kim.brandt@gotamedia.se>
2023-02-13 20:49:24 +0100

    Fixed bootstrap.sh.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,25 +2,25 @@
 
 cd "$(dirname "${BASH_SOURCE}")";
 
-git pull origin master;
+git pull origin master --quiet
 
 function Execute() {
-	rsync --exclude ".git/" \
-	      --exclude ".gitignore" \
-	      --exclude ".DS_Store" \
-	      --exclude "bootstrap.sh" \
-				--exclude "brew.sh" \
-	      -avh --no-perms . ~;
-	source ~/.bash_profile;
+    rsync --exclude ".git/" \
+	  --exclude ".gitignore" \
+	  --exclude ".DS_Store" \
+	  --exclude "bootstrap.sh" \
+	  --exclude "brew.sh" \
+	  -avh --no-perms . ~
+    source ~/.bash_profile
 }
 
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	Execute;
+    Execute
 else
-	read -p "This will overwrite all dotfiles in your home directory ($(echo ${HOME})). Are you sure? (y/n) " -n 1;
-	echo "";
-	if [[ ${REPLY} =~ ^[Yy]$ ]]; then
-		Execute;
-	fi;
-fi;
-unset Execute;
+    read -p "This will overwrite all dotfiles in your home directory ($(echo ${HOME})). Are you sure? (y/n) " -n 1
+    echo ""
+    if [[ ${REPLY} =~ ^[Yy]$ ]]; then
+	    Execute
+    fi
+fi
+unset Execute
